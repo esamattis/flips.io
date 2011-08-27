353330e39b1e78ff93eb0054c64d91785b9317a5
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-08-27 10:53:04 +0300

    add namespace too for browser

---
+++ client/namespace.js
@@ -0,0 +1,15 @@
+window.NS = function (ns_string) {
+    var parts = ns_string.split('.'),
+        parent = window,
+        i;
+
+    for (i = 0; i < parts.length; i += 1) {
+        // create a property if it doesn't exist
+        if (typeof parent[parts[i]] === "undefined") {
+            parent[parts[i]] = {};
+        }
+
+        parent = parent[parts[i]];
+    }
+    return parent;
+};
