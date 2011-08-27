bfb9c795cfd730d0065dd15fb7ed14855c93104a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-08-27 10:15:38 +0300

    listen 80 on production

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var http = require('http');
-http.createServer(function (req, res) {
+var app = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Sliders!\n');
-}).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+});
+
+app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
+  console.log('Ready');
+
+  // if run as root, downgrade to the owner of this file
+  if (process.getuid() === 0)
+    require('fs').stat(__filename, function(err, stats) {
+      if (err) return console.log(err)
+      process.setuid(stats.uid);
+    });
+});
+
